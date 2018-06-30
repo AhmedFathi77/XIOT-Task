258d5483e249f75843aea36d60aaea017830a969
Ahmed F. El-Adawy <AhmedFathi77@users.noreply.github.com>
2018-06-30 04:36:50 +0200

    Update UART.h

--- UART.h
+++ UART.h
@@ -1,14 +1,11 @@
-/*
- * UART.h
- *
- *  Created on: Jun 27, 2018
- *      Author: leenovoz510
- */
+
 
 #ifndef UART_H_
 #define UART_H_
 
-
+#define F_CPU 1000000UL //1MHz Clock frequency
+#define BaudRate 9600
+#define BAUD_PRESCALE (((F_CPU / (BaudRate * 8UL))) - 1)
 
 void UART_Init();
 void UART_Send(unsigned char data);
