02dc84ce69c80a9d927a8ae09941b6cdc8428374
Ahmed F. El-Adawy <AhmedFathi77@users.noreply.github.com>
2018-06-30 04:35:24 +0200

    Update Timer.c

--- Timer.c
+++ Timer.c
@@ -1,16 +1,19 @@
-/*
- * Timer.c
- *
- *  Created on: Jun 29, 2018
- *      Author: leenovoz510
- */
+
 #include <avr/io.h>
 
 
 
+/* ############################################################################# */
 void Overflow_TIMER0_Init(){
 	TCNT0 = 0;
 	TIMSK  |= (1<<TOIE0);			// Timer Interrupt overflow Enable pin
 	//TCCR0 |= (1<<FOC0); 			// Force output compare pin
 	TCCR0 |= (1<<FOC0) | (1<<CS00) | (1<<CS02);	// CLK 2014 From prescaler
 }
+
+/*
+	[ function name] : Overflow_TIMER0_Init
+	[ return type] : void
+	[ functionality ] : Initiate Timer0 on overflow mood
+*/
+/* ############################################################################# */
