98de931f506d568704bbb9762809bb4ee47dd363
Ahmed F. El-Adawy <AhmedFathi77@users.noreply.github.com>
2018-06-30 04:37:48 +0200

    Update Interrupt.c

--- Interrupt.c
+++ Interrupt.c
@@ -1,11 +1,9 @@
-/*
- * Interrupt.c
- *
- *  Created on: Jun 26, 2018
- *      Author: leenovoz510
- */
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
+
+
+/* ############################################################################# */
 
 void Interrupt_init(){
 	SREG  &= ~(1<<7);        // Disable interrupts by clearing I-bit
@@ -15,4 +13,9 @@ void Interrupt_init(){
 	MCUCR |=  (1<<ISC10); 	 // Any Logical change will cause an interrupt
 	SREG  |=  (1<<7);		 // Enable interrupts by setting I-bit
 }
-
+/*
+	[ function name] : Interrupt_init
+	[ return type] : void
+	[ functionality ] : initiate interrupt service routine
+*/
+/* ############################################################################# */
